2ccbd01c4e0bcb8e6eb9ef282e54c550b2d8c741
johnjihwan <s20062@gsm.hs.kr>
2021-03-12 12:29:03 +0900

    [UPDATE]edit structure

---
+++ page/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>로그인, 회원가입 페이지</title>
+</head>
+<body>
+    <div class="container">
+        <div class="login">
+            <span>로그인 섹션입니다</span>
+            <input type="text" id="id" placeholder="Email을 입력하세요">
+            <input type="password" id="password" placeholder="password를 입력하세요">
+        </div>
+        <div class="join">
+            <span>회원가입 섹션입니다</span>
+            <input type="text" id="id" placeholder="Email을 입력하세요">
+            <input type="password" id="password" placeholder="password를 입력하세요">
+        </div>
+    </div>
+</body>
+</html>
