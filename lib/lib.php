39fbf4af44bf6f91c82dc8d2ebf422e9a526928d
johnjihwan <s20062@gsm.hs.kr>
2021-03-12 16:16:56 +0900

    [UPDATE]dev before setting

---
+++ lib/lib.php
@@ -0,0 +1,60 @@
+<?php
+    $pdo = new PDO("mysql:host=loacalhost;dbname=C-Module;charset=utf8", "root", "", array(
+        PDO:: ATTR_DEFAULT_FETCH_MODE => PDO:: FETCH_OBJ,
+        PDO:: ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+    ));
+    
+    session_start(); //세션 시작
+    header("content-type: text/html; charset=utf-8"); //html docs를 한글로 인코딩
+    date_default_timezone_set("Asia/Seoul"); // 한국시간으로 설정 
+
+    //url 별로 주소 가져오기
+    $varray = isset($_GET['uri']) ? explode("/", $_GET['uri']) : NULL;
+    $pagemode = isset($varray[0]) ? $varray[0] : NULL;
+    $midx = isset($varray[1]) ? $varray[1] : 'main';
+    $page = isset($varray[2]) ? $varray[2] : 1;
+
+    // DataBase
+    function execute($sql, $parame = NULL){
+        global $pdo;
+        $data = $pdo->prepare($sql);
+        $data -> execute($parame);
+        return $data;
+    }
+    function fetch($sql, $parame = NULL){
+        return execute($sql, $parame)->fetch();
+    }
+    function fetchAll($sql, $parame = NULL){
+        return execute($sql, $parame)->fetchAll();
+    }
+    function rowCount($sql, $parame = NULL){
+        return execute($sql, $parame)->rowCount();
+    }
+    function re($data){
+        foreach($data as $key => $val){
+            if($val == NULL){
+                alert("입력을 다시 확인해주세요.");
+            }
+        }
+    }
+    // js 경고창을 php로 호출할 수 있게 하는 함수.
+    function alert($msg=NULL, $url=NULL){
+        echo "<script>";
+        echo $msg ? "alert('{$msg}');" : "";
+        echo $url ? "document.location.href='{$url}'" : "history.back()";
+        echo "</script>";
+        exit;
+    }
+
+    //login_SESSION 확인
+    $SESSIONEDMEMBERS = isset($_SESSION['member']) ? $_SESSION['member'] : NULL;
+    
+    // pagemode checking
+    if($pagemode == "action"){
+        include "./lib/action.php";
+        exit;
+    }
+    
+    //현재 memberidx 확인
+    $current_user =  $SESSIONEDMEMBERS -> idx;
+?>
--- page/index.php
+++ page/index.php
@@ -6,14 +6,16 @@
     <title>로그인, 회원가입 페이지</title>
 </head>
 <body>
+    <form class="form-member" action="../lib/action.php" method="post">
     <div class="container">
         <div class="login">
             <span>로그인 섹션입니다</span>
-            <input type="text" id="id" placeholder="Email을 입력하세요">
-            <input type="password" id="password" placeholder="password를 입력하세요">
+            <input type="text" id="id" placeholder="Email을 입력하세요" name="userId">
+            <input type="password" id="password" placeholder="password를 입력하세요" name="userPw">
         </div>
         <div class="join">
             <span>회원가입 섹션입니다</span>
+            <input type="text" id="name" placeholder="이름을 입력하세요" name="userName">
             <input type="text" id="id" placeholder="Email을 입력하세요">
             <input type="password" id="password" placeholder="password를 입력하세요">
         </div>
